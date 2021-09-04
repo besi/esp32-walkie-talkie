6ae8a8070ddb6a8e1fc8e1664aee320e8e1d1981
Beat Besmer <beat.besmer@gmail.com>
2021-09-04 11:13:19 +0200

    Adjust the pins used for I2S

--- src/config.h
+++ src/config.h
@@ -19,7 +19,7 @@
 // #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
 #define I2S_MIC_SERIAL_CLOCK GPIO_NUM_18
 #define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_19
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_17
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
@@ -30,14 +30,14 @@
 #define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_19
 #define I2S_SPEAKER_SERIAL_DATA GPIO_NUM_5
 // Shutdown line if you have this wired up or -1 if you don't
-#define I2S_SPEAKER_SD_PIN GPIO_NUM_22
+#define I2S_SPEAKER_SD_PIN GPIO_NUM_34
 
 // transmit button
 #define GPIO_TRANSMIT_BUTTON 23
 
 // Which transport do you want to use? ESP_NOW or UDP?
 // comment out this line to use UDP
-#define USE_ESP_NOW
+// #define USE_ESP_NOW
 
 // i2s config for using the internal ADC
 extern i2s_config_t i2s_adc_config;
