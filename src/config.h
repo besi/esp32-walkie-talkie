1f938e06a2ed3d164c92009f57e84fc1ac40f527
Beat Besmer <beat.besmer@gmail.com>
2024-04-03 12:17:28 +0200

    Pinout for the pico kit walkietalkie

--- src/config.h
+++ src/config.h
@@ -21,23 +21,23 @@
 // Generally they will default to LEFT - but you may need to attach the L/R pin to GND
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_18
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_19
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_17
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_33
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_32
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_36
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
 
 // speaker settings
 #define USE_I2S_SPEAKER_OUTPUT
-#define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_18
-#define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_19
+#define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_33
+#define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_32
 #define I2S_SPEAKER_SERIAL_DATA GPIO_NUM_5
 // Shutdown line if you have this wired up or -1 if you don't
-#define I2S_SPEAKER_SD_PIN GPIO_NUM_34
+#define I2S_SPEAKER_SD_PIN -1
 
 // transmit button
-#define GPIO_TRANSMIT_BUTTON 23
+#define GPIO_TRANSMIT_BUTTON 35
 
 // Which transport do you want to use? ESP_NOW or UDP?
 // comment out this line to use UDP
